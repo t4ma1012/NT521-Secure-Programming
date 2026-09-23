b9591597a7397c84282da2e3f1ce8ec770a98b6b
t4ma1012 <hakhue121006@gmail.com>
2026-09-22 22:25:05 -0400

    Nhom 12 Fix ret_val bug in json_search, 3 unit tests PASS

--- LAB1/manual/unittest/recursive_json_search.py
+++ LAB1/manual/unittest/recursive_json_search.py
@@ -1,1 +1,29 @@
 # Fill the Python code in this file
+from test_data import *
+
+def json_search(key, input_object):
+    ret_val = []
+
+    if isinstance(input_object, dict):  # Iterate dictionary
+        for k, v in input_object.items():  # searching key in the dict
+            if k == key:
+                temp = {k: v}
+                ret_val.append(temp)
+
+            if isinstance(v, dict):  # the value is another dict so repeat
+                ret_val.extend(json_search(key, v))
+
+            elif isinstance(v, list):  # it's a list
+                for item in v:
+                    if not isinstance(item, (str, int)):  # if dict or list repeat
+                        ret_val.extend(json_search(key, item))
+
+    else:  # Iterate a list because some APIs return JSON object in a list
+        for val in input_object:
+            if not isinstance(val, (str, int)):
+                ret_val.extend(json_search(key, val))
+
+    return ret_val
+
+
+print(json_search("issueSummary", data))
